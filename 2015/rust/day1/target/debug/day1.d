ce61462914ce14b99d8ef316ad73f61e2bfbe38b
Ben Hilger <bhilger78@gmail.com>
2024-01-01 00:45:10 -0500

    delete target folder for day 1

--- 2015/rust/day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/Users/benjaminhilger/Desktop/personal/adventofcode/2015/rust/day1/target/debug/day1: /Users/benjaminhilger/Desktop/personal/adventofcode/2015/rust/day1/src/main.rs
--- 2015/rust/day1/target/debug/deps/day1-3f4e3855e1f2b125.d
+++
@@ -1,5 +0,0 @@
-/Users/benjaminhilger/Desktop/personal/adventofcode/2015/rust/day1/target/debug/deps/day1-3f4e3855e1f2b125: src/main.rs
-
-/Users/benjaminhilger/Desktop/personal/adventofcode/2015/rust/day1/target/debug/deps/day1-3f4e3855e1f2b125.d: src/main.rs
-
-src/main.rs:
