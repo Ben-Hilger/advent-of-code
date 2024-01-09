0cad20deaf9d37ccd4320ca3c89dc33617cd7627
Ben Hilger <bhilger78@gmail.com>
2024-01-08 23:49:26 -0500

    continue attempt on 2015 day 7

--- 2015/rust/day7/src/main.rs
+++ 2015/rust/day7/src/main.rs
@@ -7,27 +7,29 @@ fn main() {
 
 struct Wire {
     value: i32,
-    instructions: Option<WireInstructions>
+    instructions: Option<WireInstructions>,
 }
 
 struct WireInstructions {
     wire1: String,
     instruction: String,
-    wire2: Option<String>
+    wire2: Option<String>,
 }
 
 fn part1() {
-
     let mut wires = HashMap::<String, Wire>::new();
     let lines = read_file();
 
     for line in lines {
-        let wire_operations = line
-                .split("->")
-                .collect::<Vec<_>>();
-   
+        let wire_operations = line.split("->").collect::<Vec<_>>();
+
         let wire_label = wire_operations.get(1).unwrap().trim().to_string();
-        let insturctions = wire_operations.get(0).unwrap().trim().split(" ").collect::<Vec<_>>();
+        let insturctions = wire_operations
+            .get(0)
+            .unwrap()
+            .trim()
+            .split(" ")
+            .collect::<Vec<_>>();
 
         let mut value = -1;
         let mut wire_instructions: Option<WireInstructions> = None;
@@ -35,33 +37,56 @@ fn part1() {
         if insturctions.len() == 1 {
             let is_number = insturctions.get(0).unwrap().parse::<i32>().is_ok();
             if is_number {
-                value = insturctions.get(0).unwrap().parse().expect("instructions with length 1 should be a number");        
+                value = insturctions
+                    .get(0)
+                    .unwrap()
+                    .parse()
+                    .expect("instructions with length 1 should be a number");
             } else {
                 let wire1 = insturctions.get(0).unwrap().to_string();
-                wire_instructions = Some(WireInstructions { wire1, instruction: "DIRECT".to_string(), wire2: None});
+                wire_instructions = Some(WireInstructions {
+                    wire1,
+                    instruction: "DIRECT".to_string(),
+                    wire2: None,
+                });
             }
-        } else if insturctions.len() == 2 && insturctions.get(0).unwrap().to_string() == "NOT".to_string() {
+        } else if insturctions.len() == 2
+            && insturctions.get(0).unwrap().to_string() == "NOT".to_string()
+        {
             let wire1 = insturctions.get(1).unwrap().to_string();
-            wire_instructions = Some(WireInstructions { wire1 , instruction: "NOT".to_string(), wire2: None });  
+            wire_instructions = Some(WireInstructions {
+                wire1,
+                instruction: "NOT".to_string(),
+                wire2: None,
+            });
         } else {
             let wire1 = insturctions.get(0).unwrap().to_string();
             let instruction = insturctions.get(1).unwrap().to_string();
             let wire2 = insturctions.get(2).unwrap().to_string();
-            wire_instructions = Some(WireInstructions { wire1, instruction, wire2: Some(wire2) });
+            wire_instructions = Some(WireInstructions {
+                wire1,
+                instruction,
+                wire2: Some(wire2),
+            });
         }
 
-        wires.insert(wire_label, Wire { value, instructions: wire_instructions });
+        wires.insert(
+            wire_label,
+            Wire {
+                value,
+                instructions: wire_instructions,
+            },
+        );
     }
 
     let wire_to_evaluate = "a".to_string();
 
     let result = evaluate_wire(&wire_to_evaluate, &wires);
-   
+
     println!("{}", result);
 }
 
 fn evaluate_wire(wire: &String, hash_map: &HashMap<String, Wire>) -> i32 {
-
     if let Some(wire_mapping) = hash_map.get(wire) {
         if wire_mapping.value >= 0 {
             return wire_mapping.value;
@@ -106,7 +131,7 @@ fn evaluate_wire(wire: &String, hash_map: &HashMap<String, Wire>) -> i32 {
 }
 
 fn is_number(str: &String) -> bool {
-    return str.parse::<i32>().is_ok();
+   return str.parse::<i32>().is_ok();
 }
 
 fn read_file() -> Vec<String> {
@@ -116,4 +141,3 @@ fn read_file() -> Vec<String> {
         .map(String::from)
         .collect();
 }
-
